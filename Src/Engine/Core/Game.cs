c19ced84da903b3087b76ad5ed570be147bf40bd
VonRiddarn <vonriddarn@gmail.com>
2026-09-24 12:08:27 +0200

    feat: add Draw method to game class for rendering

--- Src/Engine/Core/Game.cs
+++ Src/Engine/Core/Game.cs
@@ -9,6 +9,8 @@ abstract class Game(GameConfig config)
 	protected readonly GameConfig _gameConfig = config;
 	readonly int _tickMs = (int)MathF.Floor(1000f / config.TickRate);
 
+	readonly ConsoleBuffer _consoleBuffer = new();
+
 	bool _isRunning = false;
 
 	public void Run()
@@ -24,23 +26,27 @@ abstract class Game(GameConfig config)
 		Initialize();
 
 		// Input thread
+
 		Input.Initialize_Internal();
 
 		// Update is on main thread.
+
 		while (true)
 		{
 			Update();
-			// TODO: Renderer
+			Draw(_consoleBuffer);
 
 			Input.Clear_Cache_Internal();
 			Thread.Sleep(_tickMs);
 		}
 	}
 
+
 	/// <summary>
 	/// Entry point of your game. Use this to initialize your entities / objects.
 	/// </summary>
 	protected abstract void Initialize();
+
 
 	/// <summary>
 	/// Global update of your project. Use this to make changes each frame.
@@ -48,9 +54,12 @@ abstract class Game(GameConfig config)
 	/// </summary>
 	protected abstract void Update();
 
+	protected abstract void Draw(ConsoleBuffer buffer);
+
 	// ----- ----- -----
 	//	   HELPERS
 	// ----- ----- -----
+
 
 	static void SetupConsoleEnvironment(GameConfig config)
 	{
@@ -63,12 +72,15 @@ abstract class Game(GameConfig config)
 	static void RunSplashScreen()
 	{
 		// TODO: Allow end user to add more stuff to the splash screen dynamically...
+
 		Renderer.CursorVisible = false;
 		Console.Clear();
 		Console.ForegroundColor = ConsoleColor.DarkYellow; // (ConsoleColor)Random.Shared.Next(1, 16);
+
 		Console.WriteLine(Graphics.ENGINE_LOGO);
 
 		Console.ForegroundColor = ConsoleColor.White; // (ConsoleColor)Random.Shared.Next(1, 16);
+
 		Console.WriteLine(Graphics.ENGINGE_CREDITS);
 		Thread.Sleep(2500);
 		Console.Clear();
--- Src/Sandbox/AlienShooter.cs
+++ Src/Sandbox/AlienShooter.cs
@@ -3,6 +3,9 @@ using JankiiEngine;
 
 class AlienShooter(GameConfig config) : Game(config)
 {
+	int _x = 0;
+	int _dir = 1;
+
 	protected override void Initialize()
 	{
 		Console.WriteLine("Ready to kill some alien scum?");
@@ -11,11 +14,23 @@ class AlienShooter(GameConfig config) : Game(config)
 
 	protected override void Update()
 	{
-
 		if (Input.GetKeyDown(ConsoleKey.Spacebar))
 		{
 			Console.ForegroundColor = (ConsoleColor)Random.Shared.Next(9, 16);
 			Console.WriteLine("SPACE");
 		}
+	}
+
+	protected override void Draw(ConsoleBuffer buffer)
+	{
+		// Concern mixing, don't mind this :P
+		if ((_x + _dir) is > 120 or < 0)
+			_dir *= -1;
+
+		buffer.Clear();
+		buffer.SetCell(_x, 3, '@', _dir == 1 ? ConsoleColor.Blue : ConsoleColor.DarkYellow, ConsoleColor.Black);
+		buffer.Draw();
+
+		_x += _dir;
 	}
 }
