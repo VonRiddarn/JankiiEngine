c096ba3090f679affdcf9cc1f254b8614b61c9c5
VonRiddarn <vonriddarn@gmail.com>
2026-09-24 11:37:03 +0200

    feat: add update method for cell so that we don't have to replace cells with new instances each frame (expensive stuff)

--- Src/Engine/Core/Rendering/Cell.cs
+++ Src/Engine/Core/Rendering/Cell.cs
@@ -2,4 +2,12 @@ using System;
 
 namespace JankiiEngine;
 
-public record struct Cell(char Char, ConsoleColor FgColor, ConsoleColor BgColor);
+public record struct Cell(char Char, ConsoleColor FgColor, ConsoleColor BgColor)
+{
+	public void Update(char c, ConsoleColor fgColor, ConsoleColor bgColor)
+	{
+		Char = c;
+		FgColor = fgColor;
+		BgColor = bgColor;
+	}
+}
